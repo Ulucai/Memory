691ca1af28ecd3cffb3880ea4f9f5b1dcfcd2135
Rodrigo Andrade <rd_opo@hotmail.com>
2014-08-28 21:52:35 -0400

    Upload dos arquivos corretos

---
+++ MemoryBlock/Classes/Cell.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Microsoft.Xna.Framework;
+
+
+namespace MemoryBlock
+{
+    public class Cell
+    {
+        Rectangle bounds;
+        public Vector2 position;
+        public TextureIndex tile;
+
+        public Cell(int width, int height, Vector2 cellPosition, TextureIndex texture)
+        {
+            tile = texture;
+            position = cellPosition;
+            bounds = new Rectangle((int)position.X, (int)position.Y, width, height);
+        }
+
+        public bool Intersects(Point p)
+        {
+            Rectangle click = new Rectangle(p.X, p.Y, 1, 1);
+            if (click.Intersects(bounds))
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+    }
+}
---
+++ MemoryBlock/Classes/Map.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace MemoryBlock
+{
+    public class Map
+    {
+        /// <summary>
+        /// [Rows, Cols]
+        /// </summary>
+        Dictionary<Tuple<int, int>, Cell> grid;
+        Vector2 position;
+        int nRows, nCols;
+        public TileSet tiles;
+        int size; // vai funcionar apenas com quadrados.
+
+        public Map()
+        {
+
+        }
+
+        public Map( int rows, int cols, Vector2 mapPosition)
+        {
+            grid = new Dictionary<Tuple<int, int>, Cell>();
+            tiles = new TileSet();
+            nRows = rows;
+            nCols = cols;
+            size = 47;
+            position = mapPosition;
+
+            TextureIndex RandomIndex;
+            Random r = new Random();
+
+            for (int i = 0; i < nRows; i++)
+            {
+                
+                for (int j = 0; j < nCols; j++)
+                {                    
+                    if (r.Next(2) == 1)
+                    {
+                        RandomIndex = TextureIndex.White;
+                    }
+                    else
+                    {
+                        RandomIndex = TextureIndex.Black;
+                    }
+
+                    grid.Add(new Tuple<int, int>(j, i), 
+                             new Cell(size, 
+                                      size, 
+                                      new Vector2(position.X + (j*size), position.Y + (i * size)), 
+                                      //TextureIndex.Black));
+                                      RandomIndex));
+
+
+                }
+            }
+        }
+
+
+        public void RandomizeTiles()
+        {            
+        }
+
+
+        public void Draw(SpriteBatch sp)
+        {
+            for (int i = 0; i < nRows; i++)
+            {
+                for (int j = 0; j < nCols; j++)
+                {
+                    // TODO: find a better alternative for Dictionary + Tuple
+                    Cell c = grid[new Tuple<int,int>(j,i)];
+                    sp.Draw(tiles.texture[c.tile], c.position, Color.White);                    
+                }
+            }
+        }
+
+
+    }
+}
